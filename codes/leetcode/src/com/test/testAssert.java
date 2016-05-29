34bc060bb9fb3f15d36455332512aa4ac13e1240
Xiaotao Tang <xiaotaot@amazon.com>
2016-05-30 03:14:52 +0800

    new project

---
+++ codes/leetcode/src/com/test/testAssert.java
@@ -0,0 +1,14 @@
+package com.test;
+
+
+public class testAssert {
+    public static void main(String args[]) {
+        assert true;
+
+        System.out.println("assert true passed.");
+
+        assert false;
+
+        System.out.println("assert false passed.");
+    }
+}
--- codes/leetcode/src/com/tools/tax/TaxCalculator.java
+++ codes/leetcode/src/com/tools/tax/TaxCalculator.java
@@ -3,7 +3,7 @@ package com.tools.tax;
 
 public class TaxCalculator {
     public static void main(String[] args) {
-        TaxCalc calc = new BeijingTaxCalc(100000, 10000, 10000);
+        TaxCalc calc = new BeijingTaxCalc(30700, 10300, 169.69);
         calc.calcTax();
 
         System.out.println(Helper.formatSalaryResult(calc.salaryItems));
